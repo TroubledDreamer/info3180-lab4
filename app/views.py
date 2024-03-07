812ae73151b2ae48ce02de1c855f934f66377393
TroubledDreamer <carlyonja@gmail.com>
2024-03-07 01:36:20 -0500

    6 fixed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
@@ -36,9 +36,14 @@ def upload():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File Saved', 'success')
         return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
-
+    flash('File not Saved', 'unsuccess')
     return render_template('upload.html', form=form)
 
+
+@app.route('/files')
+def files():
+
+    return render_template("files.html" , imgs = get_uploaded_images(), urr = app.config['UPLOAD_FOLDER'])
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -96,3 +101,18 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+def get_uploaded_images():
+    upload_folder = app.config['UPLOAD_FOLDER']
+    uploaded_images = []
+    for subdir, dirs, files in os.walk(os.getcwd() + upload_folder):
+        for file in files:
+            uploaded_images.append(file)
+    return uploaded_images
+
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    upload_folder = app.config['UPLOAD_FOLDER']
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
